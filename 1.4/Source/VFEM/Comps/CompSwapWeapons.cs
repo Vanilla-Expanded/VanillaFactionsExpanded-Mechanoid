34957763001aa0cda00fea02aedd0d72c685b56c
SargBjornson <sarg.auxiliar@gmail.com>
2023-11-13 15:41:29 +0100

    v1.105

---
+++ 1.4/Source/VFEM/Comps/CompSwapWeapons.cs
@@ -0,0 +1,64 @@
+﻿
+using Verse;
+using System;
+using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+
+
+
+
+namespace VFEMech
+{
+    class CompSwapWeapons : CompAbilityEffect
+    {
+
+        public bool firstWeapon = true;
+
+        new public CompProperties_SwapWeapons Props
+        {
+            get
+            {
+                return (CompProperties_SwapWeapons)this.props;
+            } 
+        }
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+
+            if (this.parent.pawn.Faction == Faction.OfPlayer)
+            {
+                if (firstWeapon)
+                {
+                    this.parent.pawn.equipment.DestroyEquipment(this.parent.pawn.equipment.Primary);
+                    ThingDef stuff = GenStuff.RandomStuffFor(Props.weapon2);
+                    ThingWithComps newEq = (ThingWithComps)ThingMaker.MakeThing(Props.weapon2, stuff);
+                    this.parent.pawn.equipment.AddEquipment(newEq);
+                    firstWeapon = false;
+                }
+                else
+                {
+                    this.parent.pawn.equipment.DestroyEquipment(this.parent.pawn.equipment.Primary);
+                    ThingDef stuff = GenStuff.RandomStuffFor(Props.weapon1);
+                    ThingWithComps newEq = (ThingWithComps)ThingMaker.MakeThing(Props.weapon1, stuff);
+                    this.parent.pawn.equipment.AddEquipment(newEq);
+                    firstWeapon = true;
+                }
+            }
+        }
+        public override bool ShouldHideGizmo => this.parent.pawn.Faction != Faction.OfPlayer;
+
+
+        public override void PostExposeData()
+        {
+            Scribe_Values.Look(ref firstWeapon, "firstWeapon", true);
+            base.PostExposeData();
+        }
+
+
+
+
+
+    }
+}
---
+++ 1.4/Source/VFEM/Comps/Properties/CompProperties_SwapWeapons.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VFEMech
+{
+
+
+    public class CompProperties_SwapWeapons : CompProperties_AbilityEffect
+    {
+
+        public ThingDef weapon1;
+        public ThingDef weapon2;
+
+        public CompProperties_SwapWeapons()
+        {
+            this.compClass = typeof(CompSwapWeapons);
+        }
+    }
+}
